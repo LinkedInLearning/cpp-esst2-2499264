28e24247a1b500fc5e1255616f4aa6f6fb4e5e79
Gerhard Stein <gerstrong@gmail.com>
2022-09-13 11:52:24 +0200

    Chapter number adjustments, better directory naming

--- ch3/04/ThreadMutex/main.cpp
+++ ch3/04/ThreadMutex/main.cpp
@@ -4,78 +4,32 @@
 
 int main()
 {
-    // Race condition
-    {
-        std::mutex mutex;
-
-        int c=0;
-
-        std::thread t1([&] {
-            //mutex.lock();
-            std::cout << "1. Lock." << std::endl;
-
-
-            std::cout << "1. My turn!" << std::endl;
-
-            for(int i=0 ; i<1000000 ; i++)
-            {
-                c++;
-            }
-
-            std::cout << "1. Still my turn!" << std::endl;
-
-            std::cout << "1. Release" << std::endl;
-            //mutex.unlock();
-        });
-        std::thread t2([&] {
-            //mutex.lock();
-            std::cout << "2. Lock" << std::endl;
-
-            std::cout << "2. My turn!" << std::endl;
-            for(int i=0 ; i<1000000 ; i++)
-            {
-                c++;
-            }
-            std::cout << "2. Still my turn!" << std::endl;
-
-            std::cout << "2. Release" << std::endl;
-            //mutex.unlock();
-        });
-
-
-
-        t1.join();
-        t2.join();
-
-        std::cout << "c = " << c << std::endl;
-    }
-
-    return 0;
-
-    // Deadlock
-    {
-    std::mutex m1, m2;
+    std::mutex mutex;
 
     std::thread t1([&] {
-        std::cout << "1. Acquiring m1." << std::endl;
-        m1.lock();
+        mutex.lock();
+        std::cout << "1. Lock." << std::endl;
 
+
+        std::cout << "1. My turn!" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "1. Still my turn!" << std::endl;
 
-        std::cout << "1. Acquiring m2" << std::endl;
-        m2.lock();
+        std::cout << "1. Release" << std::endl;
+        mutex.unlock();
     });
-    std::thread t2([&] {
-        std::cout << "2. Acquiring m2" << std::endl;
-        m2.lock();
+    std::thread t2([&] {        
+        mutex.lock();
+        std::cout << "2. Lock" << std::endl;
 
+        std::cout << "2. My turn!" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "2. Still my turn!" << std::endl;
 
-        std::cout << "2. Acquiring m1" << std::endl;
-        m1.lock();
+        std::cout << "2. Release" << std::endl;
+        mutex.unlock();
     });
 
     t1.join();
     t2.join();
-    }
 }
